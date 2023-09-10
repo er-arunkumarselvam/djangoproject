3c39caf740c4f5054e9e8075bec9c68d16df1975
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-09-10 14:25:31 +0530

    CRUDOperation

--- CRUD_Operation/crudApp/views.py
+++ CRUD_Operation/crudApp/views.py
@@ -9,7 +9,7 @@ def read_veiw(request):
 def create_view(request):
     form =EmployeeForm()
     if request.method =='POST':
-        form.EmployeeForm(request.POST)
+        form=EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
     return render(request,'crudApp/register.html',{'form':form})
